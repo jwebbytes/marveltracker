b04f0b8b6d2222a1244bcdeac8446d0e9f1dd9a8
jwebbytes <183885944+jwebbytes@users.noreply.github.com>
2025-07-29 14:54:13 -0400

    Updating files

---
+++ src/components/Movielist.tsx
@@ -0,0 +1,23 @@
+import Movie from "./Moviecard";
+
+import movieinfo from "@/movie.json";
+
+
+function Movielist() {
+  return (
+    <div className="movielist">
+     
+        {movieinfo.map((movie) => (
+            <Movie
+            key={movie.id}
+            title={movie.name}
+            description={movie.description}
+            posterUrl={movie.link}
+           
+            />
+        ))}
+    </div>
+  );
+}
+
+export default Movielist;
