2e8784cdb9b2cc34e3ee977030a8052af6b73449
jwebbytes <183885944+jwebbytes@users.noreply.github.com>
2025-08-08 14:04:23 -0400

    title color change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <div className="home">
      
-      <h1 className="title">Welcome to the MCU Tracker</h1>
+      <h1 className="title">Marvel Tracker</h1>
       <p className="tagline">Your one-stop solution for tracking Marvel Cinematic Universe movies and shows.</p>
       <Phaseselection />
     </div>
