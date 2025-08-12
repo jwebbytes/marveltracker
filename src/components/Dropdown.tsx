7cd0ccf9ca01354d31eb08947819d26db705e74f
jwebbytes <183885944+jwebbytes@users.noreply.github.com>
2025-08-12 18:06:16 -0400

    added placeholder for dropdown

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,8 +1,8 @@
-function Dropdown(){
+function Dropdown({selectedPhase}:{selectedPhase:string}) {
     return(
         <div className="dropdown">
             <label htmlFor="phase-select">Select Phase:</label>
-            <select id="phase-select" className="dropdown-select">
+            <select id="phase-select" className="dropdown-select" onChange={(e)=>{alert("Phase selected: " + e.target.value)}}>
                 <option value="1">Phase 1</option>
                 <option value="2">Phase 2</option>
                 <option value="3">Phase 3</option>
--- src/components/Phaseselection.tsx
+++ src/components/Phaseselection.tsx
@@ -1,11 +1,17 @@
 import Dropdown from "./Dropdown";
 import Movielist from "./Movielist";
+import React, { useState } from "react";
 
+
+// Using components Dropdown and Movielist. State is passed
 function Phaseselection() {
+
+  const [selectedPhase, setSelectedPhase] = useState("1");
+
   return (
     <div className="phaseselection">
       <h2>Select a Phase</h2>
-      <Dropdown />
+      <Dropdown selectedPhase={selectedPhase} />
       <Movielist />
     </div>
   );
